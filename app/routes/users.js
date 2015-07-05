09fbad94f343c04663ceafd732634a04a41c1bba
disastrid <no_u@fuckyou.computer>
2015-07-05 12:33:08 +0100

    Users.js route updated.

--- app/routes/users.js
+++ app/routes/users.js
@@ -6,9 +6,19 @@ var router = express.Router();
  */
 router.get('/userlist', function(req, res) {
     var db = req.db;
-    var collection = db.get('userlist');
+    var collection = db.get('studyData');
     collection.find({},{},function(e,docs){
         res.json(docs);
+    });
+});
+
+router.post('/adduser', function(req, res) {
+    var db = req.db;
+    var collection = db.get('studyData'); //someData is the name of the collection!!
+    collection.insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
     });
 });
 
